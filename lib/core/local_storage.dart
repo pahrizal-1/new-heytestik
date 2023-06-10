d4b758b89362792426069f19db40276934453edc
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-06-10 21:14:09 +0700

    refactor

--- lib/core/local_storage.dart
+++ lib/core/local_storage.dart
@@ -3,43 +3,58 @@ import 'package:shared_preferences/shared_preferences.dart';
 class LocalStorage {
   Future<SharedPreferences> getPrefs() async => SharedPreferences.getInstance();
 
+  // TOKEN
   Future<void> setAccessToken({required String token}) async {
     SharedPreferences prefs = await getPrefs();
-    prefs.setString("jwtToken", token);
-  }
-
-  Future<void> setRefreshToken({required String token}) async {
-    SharedPreferences prefs = await getPrefs();
-    prefs.setString("refresh_token", token);
-  }
-
-  Future<void> setUserID({required int userID}) async {
-    SharedPreferences prefs = await getPrefs();
-    prefs.setInt("user_id", userID);
-  }
-
-  Future<void> setUsername({required String username}) async {
-    SharedPreferences prefs = await getPrefs();
-    prefs.setString("username", username);
+    prefs.setString('jwtToken', token);
   }
 
   Future<String?> getAccessToken() async {
     SharedPreferences prefs = await getPrefs();
-    return prefs.getString("jwtToken") ?? "";
+    return prefs.getString('jwtToken') ?? '-';
   }
 
-  Future<String> getUsername() async {
+  // REFRESH TOKEN
+  Future<void> setRefreshToken({required String token}) async {
     SharedPreferences prefs = await getPrefs();
-    return prefs.getString("username") ?? "-";
+    prefs.setString('refresh_token', token);
   }
 
   Future<String> getRefreshToken() async {
     SharedPreferences prefs = await getPrefs();
-    return prefs.getString("refresh_token") ?? "";
+    return prefs.getString('refresh_token') ?? '-';
+  }
+
+  // USER ID
+  Future<void> setUserID({required int userID}) async {
+    SharedPreferences prefs = await getPrefs();
+    prefs.setInt('user_id', userID);
   }
 
   Future<int?> getUserID() async {
     SharedPreferences prefs = await getPrefs();
-    return prefs.getInt("user_id") ;
+    return prefs.getInt('user_id');
+  }
+
+  // USERNAME
+  Future<void> setUsername({required String username}) async {
+    SharedPreferences prefs = await getPrefs();
+    prefs.setString('username', username);
+  }
+
+  Future<String> getUsername() async {
+    SharedPreferences prefs = await getPrefs();
+    return prefs.getString('username') ?? '-';
+  }
+
+  // ROLE
+  Future<void> setRoleID({required int roleID}) async {
+    SharedPreferences prefs = await getPrefs();
+    prefs.setInt('role_id', roleID);
+  }
+
+  Future<int?> getRoleID() async {
+    SharedPreferences prefs = await getPrefs();
+    return prefs.getInt('role_id');
   }
 }
