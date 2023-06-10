fddd68f75a1b147a6db652bccc95729cab93d88e
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-06-10 21:13:19 +0700

    reafactor LoginController

--- lib/controller/auth/login_controller.dart
+++ lib/controller/auth/login_controller.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:heystetik_mobileapps/core/error_config.dart';
+import 'package:heystetik_mobileapps/core/local_storage.dart';
 import 'package:heystetik_mobileapps/core/state_class.dart';
 import 'package:heystetik_mobileapps/service/auth/login_service.dart';
-
-import '../../core/local_storage.dart';
 
 class LoginController extends StateClass {
   TextEditingController email = TextEditingController();
@@ -16,31 +15,35 @@ class LoginController extends StateClass {
       if (email.text.isEmpty) {
         throw ErrorConfig(
           cause: ErrorConfig.userInput,
-          message: "Email harus diisi",
+          message: 'Email harus diisi',
         );
       }
       if (emailValid.hasMatch(email.text) == false) {
         throw ErrorConfig(
           cause: ErrorConfig.userInput,
-          message: "Format email salah",
+          message: 'Format email salah',
         );
       }
       if (password.text.isEmpty) {
         throw ErrorConfig(
           cause: ErrorConfig.userInput,
-          message: "Password harus diisi",
+          message: 'Password harus diisi',
         );
       }
       var data = {
-        "email": email.text,
-        "password": password.text,
+        'email': email.text,
+        'password': password.text,
       };
 
       var loginResponse = await LoginService().login(data);
       print(loginResponse);
-      LocalStorage().setAccessToken(token: loginResponse['token']);
-      LocalStorage().setUsername(username: loginResponse['data']['fullname']);
-      // if(loginResponse[''])
+      print(loginResponse['data']['token']);
+      print(loginResponse['data']['data']['fullname']);
+      print(loginResponse['data']['data']['roleId']);
+      LocalStorage().setAccessToken(token: loginResponse['data']['token']);
+      LocalStorage()
+          .setUsername(username: loginResponse['data']['data']['fullname']);
+      LocalStorage().setRoleID(roleID: loginResponse['data']['data']['roleId']);
 
       doInPost();
     });
