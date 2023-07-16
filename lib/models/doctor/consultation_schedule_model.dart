43281b3cc7f955f85a030794a4250fc241495bca
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-07-17 00:53:38 +0700

    ConsultationDoctorScheduleModel

---
+++ lib/models/doctor/consultation_schedule_model.dart
@@ -0,0 +1,88 @@
+class ConsultationDoctorScheduleModel {
+  bool? success;
+  String? message;
+  Data? data;
+
+  ConsultationDoctorScheduleModel({this.success, this.message, this.data});
+
+  ConsultationDoctorScheduleModel.fromJson(Map<String, dynamic> json) {
+    success = json['success'];
+    message = json['message'];
+    data = json['data'] != null ? Data.fromJson(json['data']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['success'] = success;
+    data['message'] = message;
+    if (this.data != null) {
+      data['data'] = this.data!.toJson();
+    }
+    return data;
+  }
+}
+
+class Data {
+  List<dynamic>? data;
+  Meta? meta;
+
+  Data({this.data, this.meta});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    // if (json['data'] != null) {
+    //   data = <Null>[];
+    //   json['data'].forEach((v) {
+    //     data!.add(Null.fromJson(v));
+    //   });
+    // }
+    meta = json['meta'] != null ? Meta.fromJson(json['meta']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    if (meta != null) {
+      data['meta'] = meta!.toJson();
+    }
+    return data;
+  }
+}
+
+class Meta {
+  int? page;
+  int? take;
+  int? itemCount;
+  int? pageCount;
+  bool? hasPreviousPage;
+  bool? hasNextPage;
+
+  Meta(
+      {this.page,
+      this.take,
+      this.itemCount,
+      this.pageCount,
+      this.hasPreviousPage,
+      this.hasNextPage});
+
+  Meta.fromJson(Map<String, dynamic> json) {
+    page = json['page'];
+    take = json['take'];
+    itemCount = json['itemCount'];
+    pageCount = json['pageCount'];
+    hasPreviousPage = json['hasPreviousPage'];
+    hasNextPage = json['hasNextPage'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['page'] = page;
+    data['take'] = take;
+    data['itemCount'] = itemCount;
+    data['pageCount'] = pageCount;
+    data['hasPreviousPage'] = hasPreviousPage;
+    data['hasNextPage'] = hasNextPage;
+    return data;
+  }
+}
