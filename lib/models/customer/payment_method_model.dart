76535e864547921cdacb5d1b1b5a357327bf0cd3
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-07-17 00:53:29 +0700

    PaymentMethodModel

---
+++ lib/models/customer/payment_method_model.dart
@@ -0,0 +1,85 @@
+class PaymentMethodModel {
+  bool? success;
+  String? message;
+  List<Data>? data;
+
+  PaymentMethodModel({this.success, this.message, this.data});
+
+  PaymentMethodModel.fromJson(Map<String, dynamic> json) {
+    success = json['success'];
+    message = json['message'];
+    if (json['data'] != null) {
+      data = <Data>[];
+      json['data'].forEach((v) {
+        data!.add(Data.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['success'] = success;
+    data['message'] = message;
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Data {
+  int? id;
+  String? name;
+  String? method;
+  String? type;
+  String? accountNumber;
+  bool? isActive;
+  dynamic createdBy;
+  dynamic updatedBy;
+  String? createdAt;
+  String? updatedAt;
+  dynamic deletedAt;
+
+  Data(
+      {this.id,
+      this.name,
+      this.method,
+      this.type,
+      this.accountNumber,
+      this.isActive,
+      this.createdBy,
+      this.updatedBy,
+      this.createdAt,
+      this.updatedAt,
+      this.deletedAt});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    method = json['method'];
+    type = json['type'];
+    accountNumber = json['account_number'];
+    isActive = json['is_active'];
+    createdBy = json['created_by'];
+    updatedBy = json['updated_by'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+    deletedAt = json['deleted_at'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['method'] = method;
+    data['type'] = type;
+    data['account_number'] = accountNumber;
+    data['is_active'] = isActive;
+    data['created_by'] = createdBy;
+    data['updated_by'] = updatedBy;
+    data['created_at'] = createdAt;
+    data['updated_at'] = updatedAt;
+    data['deleted_at'] = deletedAt;
+    return data;
+  }
+}
