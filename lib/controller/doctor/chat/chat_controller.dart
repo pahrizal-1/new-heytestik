c9134e39f5bf0747ddddef34a312bef72ff90c93
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-07-19 13:21:29 +0700

    add conditions

--- lib/controller/doctor/chat/chat_controller.dart
+++ lib/controller/doctor/chat/chat_controller.dart
@@ -21,6 +21,7 @@ class DoctorChatController extends StateClass {
   RxString startTime2 = ''.obs;
   RxString endTime1 = ''.obs;
   RxString endTime2 = ''.obs;
+
   RxBool isFirstSchedule = false.obs;
   RxBool isSecondSchedule = false.obs;
 
@@ -37,58 +38,76 @@ class DoctorChatController extends StateClass {
     await ErrorConfig.doAndSolveCatchInContext(context, () async {
       currentSchedule.value =
           await ConsultationDoctorScheduleServices().getCurrentDoctorSchedule();
+      if (currentSchedule.value!.success!) {
+        currentScheduleId.value = currentSchedule.value!.data!.id ?? 0;
+        startTime.value = currentSchedule.value!.data!.firstSchedule ?? '-';
+        endTime.value = currentSchedule.value!.data!.lastSchdule ?? '-';
 
-      currentScheduleId.value = currentSchedule.value!.data!.id ?? 0;
-      startTime.value = currentSchedule.value!.data!.firstSchedule ?? '-';
-      endTime.value = currentSchedule.value!.data!.lastSchdule ?? '-';
+        DateTime now = DateTime.now();
+        print('now $now');
 
-      startTime1.value = CurrenctTime.getFirstTime(
-        currentSchedule.value!.data!.firstSchedule.toString(),
-      );
-      print('startTime1 ${startTime1.value}');
+        String dateStr = now.toString().substring(0, 10);
+        print('tanggal $dateStr');
 
-      startTime2.value = CurrenctTime.getLastTime(
-        currentSchedule.value!.data!.firstSchedule.toString(),
-      );
-      print('startTime2 ${startTime2.value}');
+        DateTime first1 = now;
+        DateTime first2 = now;
+        DateTime second1 = now;
+        DateTime second2 = now;
 
-      endTime1.value = CurrenctTime.getFirstTime(
-        currentSchedule.value!.data!.lastSchdule.toString(),
-      );
-      print('endTime1 ${startTime2.value}');
+        // CEK APAKAH JADWAL PERTAMA ADA ATAU TIDAK
+        if (endTime.value.isNotEmpty ||
+            endTime.value != '-' ||
+            endTime.value != '') {
+          startTime1.value = CurrenctTime.getFirstTime(
+            currentSchedule.value!.data!.firstSchedule.toString(),
+          );
+          print('startTime1 ${startTime1.value}');
 
-      endTime2.value = CurrenctTime.getLastTime(
-        currentSchedule.value!.data!.lastSchdule.toString(),
-      );
-      print('endTime2 ${startTime2.value}');
+          startTime2.value = CurrenctTime.getLastTime(
+            currentSchedule.value!.data!.firstSchedule.toString(),
+          );
+          print('startTime2 ${startTime2.value}');
 
-      DateTime now = DateTime.now();
-      print('now $now');
+          first1 = DateTime.parse('$dateStr ${startTime1.value}');
+          first2 = DateTime.parse('$dateStr ${startTime2.value}');
+        }
 
-      String dateStr = now.toString().substring(0, 10);
-      print('tanggal $dateStr');
+        // CEK APAKAH JADWAL KEDUA ADA ATAU TIDAK
+        if (endTime.value.isNotEmpty ||
+            endTime.value != '-' ||
+            endTime.value != '') {
+          endTime1.value = CurrenctTime.getFirstTime(
+            currentSchedule.value!.data!.lastSchdule.toString(),
+          );
+          print('endTime1 ${startTime2.value}');
 
-      DateTime first1 = DateTime.parse('$dateStr ${startTime1.value}');
-      DateTime first2 = DateTime.parse('$dateStr ${startTime2.value}');
-      print('first1 $first1');
-      print('first2 $first2');
-      DateTime second1 = DateTime.parse('$dateStr ${endTime1.value}');
-      DateTime second2 = DateTime.parse('$dateStr ${endTime2.value}');
-      print('second1 $second1');
-      print('second2 $second2');
+          endTime2.value = CurrenctTime.getLastTime(
+            currentSchedule.value!.data!.lastSchdule.toString(),
+          );
+          print('endTime2 ${startTime2.value}');
 
-      if (now.isAfter(first1) && now.isBefore(first2)) {
-        print('jadwal pertama');
-        isFirstSchedule.value = true;
-        isSecondSchedule.value = false;
-      } else if (now.isAfter(second1) && now.isBefore(second2)) {
-        print('jadwal kedua');
-        isFirstSchedule.value = false;
-        isSecondSchedule.value = true;
-      } else {
-        print('salah');
-        isFirstSchedule.value = false;
-        isSecondSchedule.value = false;
+          second1 = DateTime.parse('$dateStr ${endTime1.value}');
+          second2 = DateTime.parse('$dateStr ${endTime2.value}');
+        }
+
+        print('first1 $first1');
+        print('first2 $first2');
+        print('second1 $second1');
+        print('second2 $second2');
+
+        if (now.isAfter(first1) && now.isBefore(first2)) {
+          print('jadwal pertama');
+          isFirstSchedule.value = true;
+          isSecondSchedule.value = false;
+        } else if (now.isAfter(second1) && now.isBefore(second2)) {
+          print('jadwal kedua');
+          isFirstSchedule.value = false;
+          isSecondSchedule.value = true;
+        } else {
+          print('salah');
+          isFirstSchedule.value = false;
+          isSecondSchedule.value = false;
+        }
       }
     });
 
