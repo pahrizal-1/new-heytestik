4ddd63d6fb627efde0be4dae06677e94752e51de
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-07-19 15:18:18 +0700

    change to order

--- lib/pages/chat_customer/select_conditions_page.dart
+++ lib/pages/chat_customer/select_conditions_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:grouped_list/grouped_list.dart';
-import 'package:heystetik_mobileapps/controller/customer/interest_conditions/interest_conditions_controller.dart';
+import 'package:heystetik_mobileapps/controller/transaction/order/order_controller.dart';
 import 'package:heystetik_mobileapps/pages/chat_customer/chat_page.dart';
 import 'package:heystetik_mobileapps/widget/loading_widget.dart';
 import '../../theme/theme.dart';
@@ -16,8 +16,7 @@ class SelectConditionsPage extends StatefulWidget {
 }
 
 class _SelectConditionsPageState extends State<SelectConditionsPage> {
-  final InterestConditionsController state =
-      Get.put(InterestConditionsController());
+  final OrderController state = Get.put(OrderController());
 
   @override
   void initState() {
