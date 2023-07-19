5c34510971925830f445b1a67f2902fe00976137
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-07-19 20:17:05 +0700

    add type data model

--- lib/service/customer/slideshow/slideshow_service.dart
+++ lib/service/customer/slideshow/slideshow_service.dart
@@ -1,16 +1,17 @@
 import 'package:heystetik_mobileapps/core/global.dart';
 import 'package:heystetik_mobileapps/core/networking_config.dart';
 import 'package:heystetik_mobileapps/core/provider_class.dart';
+import 'package:heystetik_mobileapps/models/customer/banne_model.dart';
 
 class SlideShowService extends ProviderClass {
   SlideShowService()
       : super(networkingConfig: NetworkingConfig(baseUrl: Global.BASE_API));
 
-  Future<dynamic> getSlideShow() async {
+  Future<BannerModel> getSlideShow() async {
     var response = await networkingConfig.doGet(
       '/slideshow_banner',
     );
 
-    return response;
+    return BannerModel.fromJson(response);
   }
 }
--- lib/service/customer/sniptips/sniptips_service.dart
+++ lib/service/customer/sniptips/sniptips_service.dart
@@ -1,16 +1,17 @@
 import 'package:heystetik_mobileapps/core/global.dart';
 import 'package:heystetik_mobileapps/core/networking_config.dart';
 import 'package:heystetik_mobileapps/core/provider_class.dart';
+import 'package:heystetik_mobileapps/models/customer/snips_tips_model.dart';
 
 class SnipTipsService extends ProviderClass {
   SnipTipsService()
       : super(networkingConfig: NetworkingConfig(baseUrl: Global.BASE_API));
 
-  Future<dynamic> getSnipTips() async {
+  Future<SnipsTipsModel> getSnipsTips() async {
     var response = await networkingConfig.doGet(
       '/snips_tips',
     );
 
-    return response;
+    return SnipsTipsModel.fromJson(response);
   }
 }
