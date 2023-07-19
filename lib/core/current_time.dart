9406e4748cf2d0d2d064e604772757fef3d093ce
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-07-19 12:00:21 +0700

    CurrenctTime

---
+++ lib/core/current_time.dart
@@ -0,0 +1,31 @@
+import 'package:intl/intl.dart';
+
+class CurrenctTime {
+  DateTime datetime = DateTime.now();
+
+  String timeNow() {
+    String tdata = DateFormat('HH:mm').format(datetime);
+    print('jam sekarang $tdata');
+    return tdata;
+  }
+
+  static String getFirstTime(String value) {
+    if (value.isEmpty || value == '' || value == '-') {
+      print('-');
+      return '-';
+    }
+    value = value.toString().substring(0, 5);
+    print('awal $value');
+    return value;
+  }
+
+  static String getLastTime(String value) {
+    if (value.isEmpty || value == '' || value == '-') {
+      print('-');
+      return '-';
+    }
+    value = value.toString().substring(value.length - 5);
+    print('akhir $value');
+    return value;
+  }
+}
