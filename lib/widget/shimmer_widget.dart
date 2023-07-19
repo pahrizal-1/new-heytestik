9e90d20d3a94c7b36a48db05f5c15c641f345d47
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-07-19 14:17:18 +0700

    add baseColor, highlightColor

--- lib/widget/shimmer_widget.dart
+++ lib/widget/shimmer_widget.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:heystetik_mobileapps/theme/theme.dart';
 import 'package:shimmer/shimmer.dart';
 
 Widget shimmerWidget({required Widget child}) {
   return Shimmer.fromColors(
-    baseColor: Colors.grey[300]!,
-    highlightColor: Colors.grey[100]!,
+    baseColor: baseColor,
+    highlightColor: highlightColor,
     child: child,
   );
 }
