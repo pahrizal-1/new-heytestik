be987d035b58969657687cb8970f020e53c7b96b
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-07-20 21:36:39 +0700

    add order

--- lib/service/customer/transaction/transaction_service.dart
+++ lib/service/customer/transaction/transaction_service.dart
@@ -1,18 +1,37 @@
+import 'package:dio/dio.dart';
 import 'package:heystetik_mobileapps/core/global.dart';
 import 'package:heystetik_mobileapps/core/local_storage.dart';
 import 'package:heystetik_mobileapps/core/networking_config.dart';
 import 'package:heystetik_mobileapps/core/provider_class.dart';
+import 'package:heystetik_mobileapps/models/customer/payment_method_model.dart';
 
 class TransactionService extends ProviderClass {
   TransactionService()
       : super(networkingConfig: NetworkingConfig(baseUrl: Global.BASE_API));
 
-  Future<dynamic> order(dynamic data) async {
-    var response = await networkingConfig.doPost(
-      '/transaction/consultation/order',
-      data: data,
+  Future<PaymentMethodModel> paymentMethod() async {
+    var response = await networkingConfig.doGet(
+      '/payment-method',
       headers: {
         'Authorization': 'Bearer ${await LocalStorage().getAccessToken()}'
+      },
+    );
+    return PaymentMethodModel.fromJson(response);
+  }
+
+  Future<dynamic> order(dynamic data) async {
+    FormData formData = FormData.fromMap(data);
+    for (var file in data['files']) {
+      formData.files.addAll(
+        [MapEntry('files', await MultipartFile.fromFile(file))],
+      );
+    }
+    var response = await networkingConfig.doPost(
+      '/transaction/consultation/order',
+      data: formData,
+      headers: {
+        'Authorization': 'Bearer ${await LocalStorage().getAccessToken()}',
+        'Content-type': 'multipart/form-data'
       },
     );
     return response;
