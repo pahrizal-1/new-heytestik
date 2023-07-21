63d70c6382e00bdb70444b686983f71f24ac0b64
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-07-22 01:52:08 +0700

    historyConsultation

--- lib/service/customer/transaction/transaction_service.dart
+++ lib/service/customer/transaction/transaction_service.dart
@@ -4,6 +4,7 @@ import 'package:heystetik_mobileapps/core/local_storage.dart';
 import 'package:heystetik_mobileapps/core/networking_config.dart';
 import 'package:heystetik_mobileapps/core/provider_class.dart';
 import 'package:heystetik_mobileapps/models/customer/payment_method_model.dart';
+import 'package:heystetik_mobileapps/models/customer/transaction_history_consultation_model.dart';
 
 class TransactionService extends ProviderClass {
   TransactionService()
@@ -35,5 +36,15 @@ class TransactionService extends ProviderClass {
       },
     );
     return response;
+  }
+
+  Future<TransactionHistoryConsultationModel> historyConsultation() async {
+    var response = await networkingConfig.doGet(
+      '/transaction/consultation?page=1&take=100&order=desc&search',
+      headers: {
+        'Authorization': 'Bearer ${await LocalStorage().getAccessToken()}'
+      },
+    );
+    return TransactionHistoryConsultationModel.fromJson(response);
   }
 }
