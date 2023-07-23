c4ecfd777a2dbd42a9a4e94a33e11166adf00110
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-07-23 11:39:34 +0700

    ExpiredPage

--- lib/pages/chat_customer/expired_page.dart
+++ lib/pages/chat_customer/expired_page.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:heystetik_mobileapps/pages/chat_customer/onboarding_chat_page.dart';
 import 'package:heystetik_mobileapps/pages/tabbar/tabbar_customer.dart';
 import 'package:heystetik_mobileapps/theme/theme.dart';
 
@@ -13,53 +15,66 @@ class ExpiredPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: greenColor,
-        title: Text(
-          message,
-          style: whiteTextStyle.copyWith(fontSize: 20, fontWeight: bold),
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/onboardingchat.png',
-              width: 259,
-              height: 239,
-            ),
-            Text(
-              message,
-              style: blackTextStyle.copyWith(fontSize: 25),
-            ),
-            const SizedBox(
-              height: spaceHeigt,
-            ),
-            Text(
-              'Sesi pembayaran berakhir :)',
-              style: blackRegulerTextStyle.copyWith(
-                  fontSize: 20, color: blackColor),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Padding(
-              padding: lsymetric.copyWith(bottom: 40),
-              child: ButtonGreenWidget(
-                title: 'Kembali Ke Home',
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const TabBarCustomer(),
-                    ),
-                  );
-                },
+      // appBar: AppBar(
+      //   elevation: 0,
+      //   backgroundColor: greenColor,
+      //   title: Text(
+      //     message,
+      //     style: whiteTextStyle.copyWith(fontSize: 20, fontWeight: bold),
+      //   ),
+      // ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/onboardingchat.png',
+                width: 259,
+                height: 239,
               ),
-            ),
-          ],
+              Text(
+                message,
+                style: blackTextStyle.copyWith(fontSize: 25),
+              ),
+              const SizedBox(
+                height: spaceHeigt,
+              ),
+              Text(
+                'Sesi pembayaran habis, silahkan beli konsultasi lagi',
+                textAlign: TextAlign.center,
+                style: blackRegulerTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Padding(
+                padding: lsymetric,
+                child: ButtonWhiteWidget(
+                  title: 'Konsultasi Lagi',
+                  onPressed: () async {
+                    Get.to(const OnboardingChat());
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: lsymetric.copyWith(bottom: 40),
+                child: ButtonGreenWidget(
+                  title: 'Kembali Ke Home',
+                  onPressed: () {
+                    Get.offAll(const TabBarCustomer());
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
