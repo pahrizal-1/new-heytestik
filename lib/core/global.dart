116eb8c41193f4b00f31d81e21733efca36957c9
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-07-26 13:06:00 +0700

    tst

--- lib/core/global.dart
+++ lib/core/global.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: constant_identifier_names
 
 class Global {
-  static const BASE_API = 'http://117.53.46.208:8192';
-  // static const BASE_API = 'https://heystetik.ahrulsyamil.com';
+  // static const BASE_API = 'http://117.53.46.208:8192';
+  static const BASE_API = 'https://heystetik.ahrulsyamil.com';
 }
