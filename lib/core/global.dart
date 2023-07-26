cfc3602c4fb0b9860370c07a59b6106dd5685646
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-07-26 14:49:05 +0700

    ss

--- lib/core/global.dart
+++ lib/core/global.dart
@@ -2,5 +2,6 @@
 
 class Global {
   // static const BASE_API = 'http://117.53.46.208:8192';
-  static const BASE_API = 'https://heystetik.ahrulsyamil.com';
+  // static const BASE_API = 'https://heystetik.ahrulsyamil.com';
+  static const BASE_API = 'https://f782-103-84-5-79.ngrok-free.app';
 }
