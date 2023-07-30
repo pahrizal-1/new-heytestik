2ad9893d163923b93f4f1763c66469f19c1d0c16
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-07-30 08:25:59 +0700

    selectListBank

--- lib/pages/setings&akun/rekening_bank_settings_page.dart
+++ lib/pages/setings&akun/rekening_bank_settings_page.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:heystetik_mobileapps/controller/customer/account/bank_controller.dart';
+import 'package:heystetik_mobileapps/models/customer/bank_model.dart';
 import 'package:heystetik_mobileapps/pages/setings&akun/tambah_bank_customer.dart';
+import 'package:heystetik_mobileapps/widget/alert_dialog_ulasan.dart';
 import 'package:heystetik_mobileapps/widget/button_widget.dart';
 
 import '../../theme/theme.dart';
 import '../../widget/text_form_widget.dart';
 
-class RekeningBankSettings extends StatelessWidget {
-  const RekeningBankSettings({super.key});
+class RekeningBankSettings extends StatefulWidget {
+  RekeningBankSettings({super.key});
+
+  @override
+  State<RekeningBankSettings> createState() => _RekeningBankSettingsState();
+}
+
+class _RekeningBankSettingsState extends State<RekeningBankSettings> {
+  final BankController state = Get.put(BankController());
+  @override
+  void initState() {
+    super.initState();
+    state.selectListBank(context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -69,151 +85,155 @@ class RekeningBankSettings extends StatelessWidget {
             const SizedBox(
               height: 32,
             ),
-            Row(
-              children: [
-                Image.asset(
-                  'assets/images/logo-bca.png',
-                  width: 50,
-                ),
-                const SizedBox(
-                  width: 14,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'PT. BCA (BANK CENTRAL ASIA) TBK',
-                      style: blackHigtTextStyle.copyWith(fontSize: 13),
-                    ),
-                    Text(
-                      '0211231234',
-                      style: blackRegulerTextStyle.copyWith(color: blackColor),
-                    ),
-                    Text(
-                      'a.n RINA RASMALINA',
-                      style: blackRegulerTextStyle.copyWith(color: blackColor),
-                    )
-                  ],
-                ),
-                const Spacer(),
-                Image.asset(
-                  'assets/icons/trash.png',
-                  width: 20,
-                  height: 20,
-                )
-              ],
+            Expanded(
+              child: FutureBuilder(
+                future: state.listBank(context),
+                builder: (context, AsyncSnapshot<BankModel?> snapshot) {
+                  print(snapshot.connectionState);
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: SizedBox(
+                        height: 30,
+                        width: 30,
+                        child: CircularProgressIndicator(
+                          color: greenColor,
+                        ),
+                      ),
+                    );
+                  }
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasData) {
+                      return Obx(
+                        () => state.filterData.isEmpty
+                            ? Center(
+                                child: Text(
+                                  'Tidak ada data',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontWeight: bold,
+                                    fontFamily: 'ProximaNova',
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              )
+                            : ListView.builder(
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                                itemCount: state.filterData.length,
+                                itemBuilder: (BuildContext context, index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.only(bottom: 10),
+                                    child: Row(
+                                      children: [
+                                        // Image.asset(
+                                        //   'assets/images/logo-bca.png',
+                                        //   width: 50,
+                                        // ),
+                                        // const SizedBox(
+                                        //   width: 14,
+                                        // ),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              '${state.filterData[index].bank?.name}',
+                                              style: blackHigtTextStyle
+                                                  .copyWith(fontSize: 13),
+                                            ),
+                                            Text(
+                                              '${state.filterData[index].accountNumber}',
+                                              style: blackRegulerTextStyle
+                                                  .copyWith(color: blackColor),
+                                            ),
+                                            Text(
+                                              'a.n ${state.filterData[index].name?.toUpperCase()}',
+                                              style: blackRegulerTextStyle
+                                                  .copyWith(color: blackColor),
+                                            )
+                                          ],
+                                        ),
+                                        const Spacer(),
+                                        Column(
+                                          children: [
+                                            InkWell(
+                                              onTap: () async {
+                                                showDialog(
+                                                  context: context,
+                                                  builder: (context) =>
+                                                      AlertInfomasi(
+                                                    function: () async {
+                                                      await state.deleteBank(
+                                                        context,
+                                                        state.filterData[index]
+                                                            .id!
+                                                            .toInt(),
+                                                      );
+                                                    },
+                                                  ),
+                                                );
+                                              },
+                                              child: Image.asset(
+                                                'assets/icons/trash.png',
+                                                width: 20,
+                                                height: 20,
+                                              ),
+                                            ),
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                            InkWell(
+                                              onTap: () async {
+                                                Get.to(TambahBankCustomerPage(
+                                                  isUpdate: true,
+                                                ));
+                                                await state.findBank(
+                                                  context,
+                                                  state.filterData[index].id!
+                                                      .toInt(),
+                                                );
+                                              },
+                                              child: Image.asset(
+                                                'assets/icons/edit.png',
+                                                width: 20,
+                                                height: 20,
+                                                color: greenColor,
+                                              ),
+                                            ),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  );
+                                },
+                              ),
+                      );
+                    } else {
+                      return Center(
+                        child: Text(
+                          'Tidak ada data',
+                          style: TextStyle(
+                            fontWeight: bold,
+                            fontFamily: 'ProximaNova',
+                            fontSize: 20,
+                          ),
+                        ),
+                      );
+                    }
+                  } else {
+                    return Text(
+                        'Connection State: ${snapshot.connectionState}');
+                  }
+                },
+              ),
             ),
             const Spacer(),
             ButtonGreenWidget(
               title: 'Tambah Rekening Lain',
-              onPressed: () {
-                showModalBottomSheet(
-                  isScrollControlled: true,
-                  context: context,
-                  backgroundColor: Colors.white,
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadiusDirectional.only(
-                      topEnd: Radius.circular(25),
-                      topStart: Radius.circular(25),
-                    ),
-                  ),
-                  builder: (context) => SingleChildScrollView(
-                    child: Wrap(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              top: 60, left: 33, right: 33),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Row(
-                                children: [
-                                  InkWell(
-                                    onTap: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: Image.asset(
-                                      'assets/icons/danger-icons.png',
-                                      width: 20,
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    width: 17,
-                                  ),
-                                  Text(
-                                    'Pilih Nama Bank',
-                                    style: blackHigtTextStyle.copyWith(
-                                        fontSize: 20),
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(
-                                height: 22,
-                              ),
-                              SearchTextField(
-                                title: 'Cari Kodisi',
-                              ),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              ListView.builder(
-                                shrinkWrap: true,
-                                keyboardDismissBehavior:
-                                    ScrollViewKeyboardDismissBehavior.onDrag,
-                                physics: const NeverScrollableScrollPhysics(),
-                                itemCount: 10,
-                                itemBuilder: (BuildContext context, int index) {
-                                  return Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      const SizedBox(
-                                        height: 20,
-                                      ),
-                                      InkWell(
-                                        onTap: () {
-                                          showModalBottomSheet(
-                                              isScrollControlled: true,
-                                              context: context,
-                                              backgroundColor:
-                                                  Colors.transparent,
-                                              shape:
-                                                  const RoundedRectangleBorder(
-                                                borderRadius:
-                                                    BorderRadiusDirectional
-                                                        .only(
-                                                  topEnd: Radius.circular(25),
-                                                  topStart: Radius.circular(25),
-                                                ),
-                                              ),
-                                              builder: (context) =>
-                                                  const TambahBankCustomerPage());
-                                        },
-                                        child: Text(
-                                          'PT BANK RAKYAT INDONESIA (PERSERO) Tbk',
-                                          style: blackHigtTextStyle.copyWith(
-                                              fontSize: 14),
-                                        ),
-                                      ),
-                                      const SizedBox(
-                                        height: 16,
-                                      ),
-                                      const Divider(
-                                        thickness: 2,
-                                      )
-                                    ],
-                                  );
-                                },
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
+              onPressed: () async {
+                // clear search
+                state.clearForm();
+                Get.to(TambahBankCustomerPage());
               },
             ),
             const SizedBox(
