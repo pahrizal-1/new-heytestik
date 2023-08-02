c40a022510bcbc4037bbb52d18a63ad2b55394e9
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-08-03 02:09:16 +0700

    ProfileController

---
+++ lib/controller/customer/account/profile_controller.dart
@@ -0,0 +1,55 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:heystetik_mobileapps/core/error_config.dart';
+import 'package:heystetik_mobileapps/core/local_storage.dart';
+import 'package:heystetik_mobileapps/core/state_class.dart';
+import 'package:heystetik_mobileapps/pages/auth/login_page.dart';
+import 'package:heystetik_mobileapps/service/customer/profile/profile_service.dart';
+
+class ProfileController extends StateClass {
+  RxString fullName = '-'.obs;
+  Map dataUser = {};
+
+  init() async {
+    fullName.value = await LocalStorage().getFullName();
+    dataUser = await LocalStorage().getDataUser();
+
+    print('fullname ${fullName.value}');
+    print('dataUser $dataUser');
+    print('dataUser ${dataUser['fullname']}');
+  }
+
+  closeAccount(BuildContext context, {required Function() doInPost}) async {
+    isLoading.value = true;
+    await ErrorConfig.doAndSolveCatchInContext(context, () async {
+      var res = await ProfileService().closedAccount();
+      if (res['success'] != true && res['message'] != 'Success') {
+        throw ErrorConfig(
+          cause: ErrorConfig.anotherUnknow,
+          message: res['message'],
+        );
+      }
+
+      doInPost();
+    });
+    isLoading.value = false;
+  }
+
+  logout(BuildContext context) async {
+    await ErrorConfig.doAndSolveCatchInContext(context, () async {
+      await LocalStorage().removeAccessToken();
+      int userID = await LocalStorage().getUserID() ?? 0;
+      WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+        await FirebaseMessaging.instance.unsubscribeFromTopic('all');
+        await FirebaseMessaging.instance
+            .unsubscribeFromTopic(userID.toString());
+      });
+
+      Get.offAll(() => const LoginPage());
+      print('logout customer');
+    });
+  }
+}
