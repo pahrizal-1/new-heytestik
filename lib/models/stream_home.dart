a25ffb49eb0ca0c2ed806712b2d0fe94d065ae4f
pahriza-1 <pahrizal480@gmail.com>
2023-08-02 21:17:01 +0700

    [UPDATE] update treatment

---
+++ lib/models/stream_home.dart
@@ -0,0 +1,59 @@
+class StreamHomeModel {
+  final String id;
+  final String content;
+  final String type;
+  final String createdAt;
+  final String visibility;
+  final List<String> streamHashtags;
+  final List<String> streamPollOptions;
+  final String fullname;
+  final String photoUser;
+  final int streamSaves;
+  final int streamComments;
+  final int streamLikes;
+  final int streamCommentReplies;
+  final bool saved;
+  final bool liked;
+
+  const StreamHomeModel({
+    required this.id,
+    required this.content,
+    required this.type,
+    required this.createdAt,
+    required this.visibility,
+    required this.streamHashtags,
+    required this.streamPollOptions,
+    required this.fullname,
+    required this.photoUser,
+    required this.streamSaves,
+    required this.streamComments,
+    required this.streamLikes,
+    required this.streamCommentReplies,
+    required this.saved,
+    required this.liked,
+  });
+
+  factory StreamHomeModel.fromJson(Map<String, dynamic> json) {
+    return StreamHomeModel(
+      id: json['id'],
+      content: json['content'],
+      type: json['type'],
+      createdAt: json['created_at'],
+      visibility: json['visibility'],
+      streamHashtags: (json['stream_hastags'] as List)
+          .map((e) => "#${e['hashtag']['tag']}")
+          .toList(),
+      streamPollOptions: json['stream_poll'] == null ? [] : (json['stream_poll']['stream_poll_options'] as List)
+          .map((e) => e['option'].toString())
+          .toList(),
+      fullname: json['user']['fullname'],
+      photoUser: json['user']['photo_profile'],
+      streamSaves: json['_count']['stream_saves'],
+      streamComments: json['_count']['stream_comments'],
+      streamLikes: json['_count']['stream_likes'],
+      streamCommentReplies: json['_count']['stream_comment_replies'],
+      saved: json['saved'],
+      liked: json['liked'],
+    );
+  }
+}
--- lib/pages/solution/view_detail_treatment_page.dart
+++ lib/pages/solution/view_detail_treatment_page.dart
@@ -576,7 +576,9 @@ class _BokingTreatmentState extends State<BokingTreatment> {
                             Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => UlasanPage()));
+                                    builder: (context) => UlasanPage(
+                                      treatmentID: widget.treatment.id!,
+                                    )));
                           },
                           child: Text(
                             'Lihat Semua',
