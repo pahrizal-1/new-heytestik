bf773843e55729c5b333fdcb96c226f2c9b80dab
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-08-05 13:21:10 +0700

    waitingReview

--- lib/pages/setings&akun/ulasan_settings_page.dart
+++ lib/pages/setings&akun/ulasan_settings_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:heystetik_mobileapps/controller/customer/account/review_controller.dart';
+import 'package:heystetik_mobileapps/core/convert_date.dart';
+import 'package:heystetik_mobileapps/core/global.dart';
 import 'package:heystetik_mobileapps/pages/setings&akun/tulis_ulasan_page.dart';
-
+import 'package:heystetik_mobileapps/models/customer/waiting_review_model.dart';
+import 'package:heystetik_mobileapps/widget/loading_widget.dart';
 import '../../theme/theme.dart';
 import '../../widget/appbar_widget.dart';
 import '../../widget/ulasan_riwayat_widgets.dart';
@@ -15,7 +20,34 @@ class UlasanSetingsPage extends StatefulWidget {
 }
 
 class _UlasanSetingsPageState extends State<UlasanSetingsPage> {
+  final ReviewController state = Get.put(ReviewController());
+  final ScrollController scrollController = ScrollController();
+  List<Data2> waitingReview = [];
+  int page = 1;
   int isSelected = 0;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      waitingReview.addAll(await state.waitingReview(context, page));
+      setState(() {});
+    });
+
+    scrollController.addListener(() {
+      if (scrollController.position.atEdge) {
+        bool isTop = scrollController.position.pixels == 0;
+        if (!isTop) {
+          page += 1;
+          WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+            waitingReview.addAll(await state.waitingReview(context, page));
+            setState(() {});
+          });
+        }
+      }
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,7 +90,7 @@ class _UlasanSetingsPageState extends State<UlasanSetingsPage> {
                               borderRadius: BorderRadius.circular(7)),
                           child: Center(
                             child: Text(
-                              'Menunggu diulas (10)',
+                              'Menunggu diulas (${waitingReview.length})',
                               style: isSelected == 0
                                   ? grenTextStyle.copyWith(
                                       fontSize: 15,
@@ -112,50 +144,77 @@ class _UlasanSetingsPageState extends State<UlasanSetingsPage> {
       body: isSelected == 0
           ? Padding(
               padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
-              child: ListView(
-                children: [
-                  UlasanProuduk(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (_) => const TulisUlasaPage()));
-                    },
-                    nameProduk: 'ISISPHARMA',
-                    tanggal: '12 Jun 2023',
-                    titleButton: 'Beli Lagi',
-                    img: 'assets/images/penting1.png',
-                    namabrand: 'Teenderm Hydra 40ml',
-                  ),
-                  const UlasanProuduk(
-                    nameProduk: 'Peeling TCA Ringan',
-                    tanggal: '12 Jun 2023',
-                    titleButton: 'Beli Lagi',
-                    img: 'assets/images/treat1.png',
-                    namabrand: 'Klinik Utama Lithea',
-                  ),
-                  const UlasanProuduk(
-                    nameProduk: 'dr. Risty Hafinah, Sp.DV',
-                    tanggal: '12 Jun 20023',
-                    titleButton: 'Beli Lagi',
-                    img: 'assets/images/doctor-img.png',
-                    namabrand: 'Konsultasi',
-                  ),
-                  const UlasanProuduk(
-                    nameProduk: 'dr. Risty Hafinah, Sp.DV',
-                    tanggal: '12 Jun 20023',
-                    titleButton: 'Beli Lagi',
-                    img: 'assets/images/doctor-img.png',
-                    namabrand: 'Konsultasi',
-                  ),
-                  const UlasanProuduk(
-                    nameProduk: 'dr. Risty Hafinah, Sp.DV',
-                    tanggal: '12 Jun 20023',
-                    titleButton: 'Beli Lagi',
-                    img: 'assets/images/doctor-img.png',
-                    namabrand: 'Konsultasi',
-                  ),
-                ],
+              child: Obx(
+                () => LoadingWidget(
+                  isLoading: state.isLoading.value,
+                  child: waitingReview.isEmpty
+                      ? Center(
+                          child: Text(
+                            'Belum ada data',
+                            style: TextStyle(
+                              fontWeight: bold,
+                              fontFamily: 'ProximaNova',
+                              fontSize: 20,
+                            ),
+                          ),
+                        )
+                      : SingleChildScrollView(
+                          controller: scrollController,
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: waitingReview.length,
+                            itemBuilder: (BuildContext context, index) {
+                              if (waitingReview[index].transactionType ==
+                                  'CONSULTATION') {
+                                return UlasanProudukKonsultasi(
+                                  onPressed: () {
+                                    Get.to(TulisUlasaPage());
+                                  },
+                                  nameProduk: 'nama prod',
+                                  tanggal: ConvertDate.defaultDate(
+                                      waitingReview[index].createdAt ?? '-'),
+                                  // titleButton: 'Beli Lagi',
+                                  img: waitingReview[index]
+                                              .detail
+                                              ?.consultation ==
+                                          null
+                                      ? '-'
+                                      : '${Global.FILE}/${waitingReview[index].detail?.consultation?.doctor!.mediaUserProfilePicture?.media?.path}',
+                                  namabrand: 'Teenderm Hydra 40ml',
+                                );
+                              }
+                              if (waitingReview[index].transactionType ==
+                                  'TREATMENT') {
+                                return UlasanProudukTreatment(
+                                  onPressed: () {
+                                    Get.to(TulisUlasaPage());
+                                  },
+                                  item: waitingReview[index]
+                                      .detail
+                                      ?.treatment
+                                      ?.mediaTreatments,
+                                  nameProduk: waitingReview[index]
+                                          .detail
+                                          ?.treatment
+                                          ?.name ??
+                                      '-',
+                                  tanggal: ConvertDate.defaultDate(
+                                      waitingReview[index].createdAt ?? '-'),
+                                  // titleButton: 'Beli Lagi',
+                                  // img: 'assets/images/penting1.png',
+                                  klinik: waitingReview[index]
+                                          .detail
+                                          ?.treatment
+                                          ?.clinic
+                                          ?.name ??
+                                      '-',
+                                );
+                              }
+                            },
+                          ),
+                        ),
+                ),
               ),
             )
           : SingleChildScrollView(
@@ -167,9 +226,11 @@ class _UlasanSetingsPageState extends State<UlasanSetingsPage> {
                   TextUlasanRiwayat(
                     onPressed: () {
                       Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const DetailPageUlasan()));
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const DetailPageUlasan(),
+                        ),
+                      );
                     },
                     nameBrand: 'ISISPHARMA',
                     nameProduk: 'Teenderm Hydra 40ml',
@@ -208,3 +269,31 @@ class _UlasanSetingsPageState extends State<UlasanSetingsPage> {
     );
   }
 }
+  // const UlasanProuduk(
+                          //   nameProduk: 'Peeling TCA Ringan',
+                          //   tanggal: '12 Jun 2023',
+                          //   titleButton: 'Beli Lagi',
+                          //   img: 'assets/images/treat1.png',
+                          //   namabrand: 'Klinik Utama Lithea',
+                          // ),
+                          // const UlasanProuduk(
+                          //   nameProduk: 'dr. Risty Hafinah, Sp.DV',
+                          //   tanggal: '12 Jun 20023',
+                          //   titleButton: 'Beli Lagi',
+                          //   img: 'assets/images/doctor-img.png',
+                          //   namabrand: 'Konsultasi',
+                          // ),
+                          // const UlasanProuduk(
+                          //   nameProduk: 'dr. Risty Hafinah, Sp.DV',
+                          //   tanggal: '12 Jun 20023',
+                          //   titleButton: 'Beli Lagi',
+                          //   img: 'assets/images/doctor-img.png',
+                          //   namabrand: 'Konsultasi',
+                          // ),
+                          // const UlasanProuduk(
+                          //   nameProduk: 'dr. Risty Hafinah, Sp.DV',
+                          //   tanggal: '12 Jun 20023',
+                          //   titleButton: 'Beli Lagi',
+                          //   img: 'assets/images/doctor-img.png',
+                          //   namabrand: 'Konsultasi',
+                          // ),
