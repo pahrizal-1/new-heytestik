8259053c54ff4d3e73f625504eff7bf8b8665728
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-08-18 17:54:52 +0700

    update

--- lib/core/global.dart
+++ lib/core/global.dart
@@ -2,7 +2,7 @@
 
 class Global {
   // static const BASE_API = 'http://192.168.0.118:8193';
-  // static const BASE_API = 'http://117.53.46.208:8192';
-  static const BASE_API = 'https://cc71-114-124-242-38.ngrok-free.app';
+  static const BASE_API = 'http://117.53.46.208:8192';
+  // static const BASE_API = 'https://cc71-114-124-242-38.ngrok-free.app';
   static const FILE = '$BASE_API/files';
 }
