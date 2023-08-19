e00cdc67368ed59730daddf566d7e69dcdd4084d
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-08-19 13:58:01 +0700

    unduh dokter note done

--- lib/core/download_file.dart
+++ lib/core/download_file.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
+import 'dart:math';
 
+import 'package:flutter_file_dialog/flutter_file_dialog.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart' as path_provider;
 import 'package:http/http.dart' as http;
 
-Future downloadFile(String url) async {
+Future downloadFileFromUrl(String url) async {
   try {
     final response = await http.get(Uri.parse(url));
 
@@ -26,3 +28,41 @@ Future downloadFile(String url) async {
     print("error download file $e");
   }
 }
+
+downloadFile(List<int> image) async {
+  try {
+    // Get temporary directory
+    final dir = await path_provider.getTemporaryDirectory();
+
+    // Create an image name
+    var filename = '${dir.path}/image-${getRandomString(5)}.png';
+
+    // Save to filesystem
+    final file = File(filename);
+    await file.writeAsBytes(image);
+
+    // Ask the user to save it
+    final params = SaveFileDialogParams(sourceFilePath: file.path);
+    final finalPath = await FlutterFileDialog.saveFile(params: params);
+
+    if (finalPath != null) {
+      print('Image saved to disk');
+    } else {
+      print("tidak tersimpan");
+    }
+  } catch (e) {
+    print("error download file $e");
+  }
+}
+
+const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+Random _rnd = Random();
+
+String getRandomString(int length) => String.fromCharCodes(
+      Iterable.generate(
+        length,
+        (_) => _chars.codeUnitAt(
+          _rnd.nextInt(_chars.length),
+        ),
+      ),
+    );
