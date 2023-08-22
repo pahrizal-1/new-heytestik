7e1c1dbb216fef00ac30e4d9f11b88e1b8ec67ba
ikhsrf <ikhsan3568@gmail.com>
2023-08-22 10:10:54 +0700

    [UPDATE] update login with google

--- lib/pages/auth/login_page_new.dart
+++ lib/pages/auth/login_page_new.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:heystetik_mobileapps/pages/auth/login_google_page.dart';
 import 'package:heystetik_mobileapps/pages/auth/phone_number_page.dart';
 import 'package:heystetik_mobileapps/pages/bantuan_mihey/home_bantuan_minhey_page.dart';
 
+import '../../controller/auth/login_controller.dart';
 import '../../theme/theme.dart';
 import '../../widget/appbar_widget.dart';
 import '../../widget/button_widget.dart';
 
-class LoginPageNew extends StatelessWidget {
+class LoginPageNew extends StatefulWidget {
   const LoginPageNew({super.key});
+
+  @override
+  State<LoginPageNew> createState() => _LoginPageNewState();
+}
+
+class _LoginPageNewState extends State<LoginPageNew> {
+  final LoginController state = Get.put(LoginController());
+  bool usernameIsFilled = false;
 
   @override
   Widget build(BuildContext context) {
@@ -27,13 +38,15 @@ class LoginPageNew extends StatelessWidget {
             Center(
               child: RichText(
                 text: TextSpan(
-                    text: 'Masuk Ke',
-                    style: blackHigtTextStyle.copyWith(fontSize: 24),
-                    children: [
-                      TextSpan(
-                          text: ' Heystetik',
-                          style: grenTextStyle.copyWith(fontSize: 24))
-                    ]),
+                  text: 'Masuk Ke',
+                  style: blackHigtTextStyle.copyWith(fontSize: 24),
+                  children: [
+                    TextSpan(
+                      text: ' Heystetik',
+                      style: grenTextStyle.copyWith(fontSize: 24),
+                    )
+                  ],
+                ),
               ),
             ),
             SizedBox(
@@ -42,6 +55,7 @@ class LoginPageNew extends StatelessWidget {
             SizedBox(
               height: 40,
               child: TextFormField(
+                controller: state.email,
                 decoration: InputDecoration(
                   labelText: 'Email atau Nomor Handphone',
                   labelStyle: blackTextStyle.copyWith(fontSize: 12),
@@ -57,8 +71,10 @@ class LoginPageNew extends StatelessWidget {
                     ),
                     borderRadius: BorderRadius.circular(10),
                   ),
-                  contentPadding:
-                      const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 12,
+                  ),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
@@ -73,109 +89,134 @@ class LoginPageNew extends StatelessWidget {
               ' Contoh: 085211233456',
               style: subTitleTextStyle.copyWith(fontSize: 13),
             ),
+            if (usernameIsFilled) ...[
+              SizedBox(
+                height: 16,
+              ),
+              SizedBox(
+                height: 40,
+                child: TextFormField(
+                  controller: state.password,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: blackTextStyle.copyWith(fontSize: 12),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: borderColor,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: borderColor,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 12,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                  ),
+                ),
+              ),
+            ],
             SizedBox(
               height: 2,
             ),
             InkWell(
               onTap: () {
                 showModalBottomSheet(
-                    backgroundColor: Colors.white,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadiusDirectional.only(
-                        topEnd: Radius.circular(25),
-                        topStart: Radius.circular(25),
+                  backgroundColor: Colors.white,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadiusDirectional.only(
+                      topEnd: Radius.circular(25),
+                      topStart: Radius.circular(25),
+                    ),
+                  ),
+                  context: context,
+                  builder: (context) => Container(
+                    height: 246,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 33, top: 47, right: 45),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Lupa Kata Sandi?',
+                            style: blackTextStyle.copyWith(fontSize: 20),
+                          ),
+                          const SizedBox(
+                            height: 28,
+                          ),
+                          Text(
+                            'Kami akan kirimkan Kata Sandi Anda\nke email yang terdaftar di akun Heystetik.',
+                            style: blackHigtTextStyle.copyWith(fontWeight: regular, fontSize: 15),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: InkWell(
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Container(
+                                    height: 34,
+                                    decoration: BoxDecoration(
+                                      color: whiteColor,
+                                      border: Border.all(color: greenColor),
+                                      borderRadius: BorderRadius.circular(7),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        'Batal',
+                                        style: TextStyle(color: greenColor, fontSize: 15),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 12,
+                              ),
+                              Expanded(
+                                child: InkWell(
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Container(
+                                    height: 34,
+                                    decoration: BoxDecoration(
+                                      color: greenColor,
+                                      borderRadius: BorderRadius.circular(7),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        'Lanjut',
+                                        style: TextStyle(
+                                          color: whiteColor,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
-                    context: context,
-                    builder: (context) => Container(
-                          height: 246,
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                                left: 33, top: 47, right: 45),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Lupa Kata Sandi?',
-                                  style: blackTextStyle.copyWith(fontSize: 20),
-                                ),
-                                const SizedBox(
-                                  height: 28,
-                                ),
-                                Text(
-                                  'Kami akan kirimkan Kata Sandi Anda\nke email yang terdaftar di akun Heystetik.',
-                                  style: blackHigtTextStyle.copyWith(
-                                      fontWeight: regular, fontSize: 15),
-                                ),
-                                const SizedBox(
-                                  height: 15,
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Expanded(
-                                      child: InkWell(
-                                        onTap: () {
-                                          // Navigator.push(
-                                          //   context,
-                                          //   MaterialPageRoute(
-                                          //     builder: (context) => const SearchKlinkPage(),
-                                          //   ),
-                                          // );
-                                        },
-                                        child: Container(
-                                          height: 34,
-                                          // width: MediaQuery.of(context).size.width,
-                                          decoration: BoxDecoration(
-                                            color: whiteColor,
-                                            border:
-                                                Border.all(color: greenColor),
-                                            borderRadius:
-                                                BorderRadius.circular(7),
-                                          ),
-                                          child: Center(
-                                            child: Text('Batal',
-                                                style: TextStyle(
-                                                    color: greenColor,
-                                                    fontSize: 15)),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      width: 12,
-                                    ),
-                                    Expanded(
-                                      child: InkWell(
-                                        onTap: () {
-                                          Navigator.pop(context);
-                                        },
-                                        child: Container(
-                                          height: 34,
-                                          // width: MediaQuery.of(context).size.width,
-                                          decoration: BoxDecoration(
-                                            color: greenColor,
-                                            borderRadius:
-                                                BorderRadius.circular(7),
-                                          ),
-                                          child: Center(
-                                            child: Text(
-                                              'Lanjut',
-                                              style: TextStyle(
-                                                  color: whiteColor,
-                                                  fontSize: 15),
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ));
+                  ),
+                );
               },
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -183,8 +224,7 @@ class LoginPageNew extends StatelessWidget {
                 children: [
                   Text(
                     'Lupa Kata Sandi',
-                    style: grenTextStyle.copyWith(
-                        fontSize: 14, fontWeight: regular),
+                    style: grenTextStyle.copyWith(fontSize: 14, fontWeight: regular),
                   )
                 ],
               ),
@@ -193,7 +233,18 @@ class LoginPageNew extends StatelessWidget {
               height: 12,
             ),
             ButtonGreenWidget(
-              title: 'Selanjutnya',
+              onPressed: () async {
+                if (usernameIsFilled) {
+                  await state.logIn(context, doInPost: () async {
+                    await state.redirectTo();
+                  });
+                } else {
+                  setState(() {
+                    usernameIsFilled = true;
+                  });
+                }
+              },
+              title: usernameIsFilled ? "Login" : 'Selanjutnya',
             ),
             SizedBox(
               height: 33,
@@ -202,11 +253,12 @@ class LoginPageNew extends StatelessWidget {
               children: <Widget>[
                 Expanded(
                   child: Container(
-                      margin: const EdgeInsets.only(left: 10.0, right: 20.0),
-                      child: Divider(
-                        color: greyColor,
-                        height: 36,
-                      )),
+                    margin: const EdgeInsets.only(left: 10.0, right: 20.0),
+                    child: Divider(
+                      color: greyColor,
+                      height: 36,
+                    ),
+                  ),
                 ),
                 Text(
                   'atau daftar dengan',
@@ -227,6 +279,14 @@ class LoginPageNew extends StatelessWidget {
               height: 23,
             ),
             InkWell(
+              onTap: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoginGooglePage(),
+                  ),
+                );
+              },
               child: Image.asset(
                 'assets/images/Frame 70.png',
               ),
