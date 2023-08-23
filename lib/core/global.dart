208a967ad633f51d89c2e977105168d0c0773d56
Yusuf Ibrohim <yusufibro24@gmail.com>
2023-08-23 18:25:10 +0700

    image chat

--- lib/core/global.dart
+++ lib/core/global.dart
@@ -3,6 +3,6 @@
 class Global {
   // static const BASE_API = 'http://192.168.0.118:8193';
   // static const BASE_API = 'http://117.53.46.208:8192';
-  static const BASE_API = 'https://2400-103-84-5-5.ngrok-free.app';
+  static const BASE_API = 'https://b68a-103-145-244-10.ngrok-free.app';
   static const FILE = '$BASE_API/files';
 }
