d65ee843d4cc0b9cba5e7e9bb26fb18f9c5561c7
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-09-16 12:39:26 +0700

    AlertSuccessWishlist

--- lib/pages/setings&akun/wishlist_page.dart
+++ lib/pages/setings&akun/wishlist_page.dart
@@ -269,6 +269,7 @@ class _WishListPageState extends State<WishListPage> {
                                 ),
                               );
                             }
+                            return null;
                           },
                         ),
                 ),
