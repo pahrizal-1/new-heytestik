c349e8b8bade9c3e9f15ac659ee190ff2ae3e734
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-09-17 22:26:32 +0700

    toJson

--- lib/models/customer/stream_post.dart
+++ lib/models/customer/stream_post.dart
@@ -4,6 +4,7 @@ class StreamPostModel {
   final List<String?> hashtags;
   final DateTime endTime;
   final List<String> options;
+  final String visibility;
 
   const StreamPostModel({
     required this.content,
@@ -11,5 +12,17 @@ class StreamPostModel {
     required this.hashtags,
     required this.endTime,
     required this.options,
+    required this.visibility,
   });
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['content'] = content;
+    data['type'] = type;
+    data['hashtags'] = hashtags;
+    data['options'] = options;
+    data['endTime'] = endTime;
+    data['visibility'] = visibility;
+    return data;
+  }
 }
