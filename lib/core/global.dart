059c0999f54e82ee5ac079d09d2323e030b4b988
Yusuf Ibrohim <yusufibro24@gmail.com>
2023-09-26 23:37:50 +0700

    update verify otp change password customer

--- lib/core/global.dart
+++ lib/core/global.dart
@@ -2,7 +2,7 @@
 
 class Global {
   // static const BASE_API = 'http://192.168.0.118:8193';
-  static const BASE_API = 'http://117.53.46.208:8192';
-  // static const BASE_API = 'https://b68a-103-145-244-10.ngrok-free.app';
+  // static const BASE_API = 'http://117.53.46.208:8192';
+  static const BASE_API = 'https://0a9c-114-124-236-69.ngrok-free.app';
   static const FILE = '$BASE_API/files';
 }
--- lib/service/auth/change_password_service.dart
+++ lib/service/auth/change_password_service.dart
@@ -1,6 +1,8 @@
 import '../../core/provider_class.dart';
 import 'package:heystetik_mobileapps/core/global.dart';
 import 'package:heystetik_mobileapps/core/networking_config.dart';
+import 'package:ua_client_hints/ua_client_hints.dart';
+
 
 class ChangePasswordService extends ProviderClass {
   ChangePasswordService()
@@ -10,6 +12,18 @@ class ChangePasswordService extends ProviderClass {
     var response = await networkingConfig.doUpdate(
       '/auth/change-password',
       data,
+    );
+
+    return response;
+  }
+
+  Future<dynamic> verifyOTP(dynamic data) async {
+    var response = await networkingConfig.doPost(
+      '/verification/verify',
+      data: data,
+      headers: {
+        'User-Agent': await userAgent(),
+      },
     );
 
     return response;
