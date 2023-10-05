a6bc1658fdeafbecd779d533ae11a5cf4733b2d6
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-10-05 16:16:49 +0700

    update

--- lib/pages/chat_customer/hasil_poto_wajah_depan_pertanyaan.dart
+++ lib/pages/chat_customer/hasil_poto_wajah_depan_pertanyaan.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:heystetik_mobileapps/controller/customer/account/my_journey_controller.dart';
 import 'package:heystetik_mobileapps/pages/chat_customer/camera_wajah_kiri_preasesment.dart';
-import 'package:heystetik_mobileapps/pages/myJourney/cutome_poto_wajah_kanan.dart';
 import 'package:heystetik_mobileapps/pages/myJourney/hasil_poto_wajah_kanan.dart';
 import 'package:heystetik_mobileapps/theme/theme.dart';
 import 'package:image_cropper/image_cropper.dart';
