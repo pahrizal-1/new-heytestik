9bcf5cf9bc4c6a6e170eba6109e136e6117e22fd
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-10-05 14:25:13 +0700

    update

--- lib/pages/profile_costumer/edit_beauty_profil_page.dart
+++ lib/pages/profile_costumer/edit_beauty_profil_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:heystetik_mobileapps/controller/customer/account/profile_controller.dart';
 import 'package:heystetik_mobileapps/pages/auth/beauty_profile_page.dart';
-import 'package:heystetik_mobileapps/pages/auth/skin_goals_tubuh.dart';
 import 'package:heystetik_mobileapps/pages/auth/skin_goals_wajah.dart';
 import 'package:heystetik_mobileapps/theme/theme.dart';
 import 'package:heystetik_mobileapps/widget/appbar_widget.dart';
 
 class EditBeautyProfilPage extends StatelessWidget {
-  const EditBeautyProfilPage({super.key});
-
+  EditBeautyProfilPage({super.key});
+  final ProfileController state = Get.put(ProfileController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,10 +20,12 @@ class EditBeautyProfilPage extends StatelessWidget {
                   left: 31, right: 31, top: 45, bottom: 28),
               child: Column(
                 children: [
-                  Text(
-                    'RINA’S PROFILE',
-                    style: blackTextStyle.copyWith(
-                      fontSize: 16,
+                  Obx(
+                    () => Text(
+                      '${state.fullName.value.toUpperCase()} PROFILE',
+                      style: blackTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
                     ),
                   ),
                   SizedBox(
@@ -126,30 +128,38 @@ class EditBeautyProfilPage extends StatelessWidget {
                   SizedBox(
                     height: 29,
                   ),
-                  skingoalstext('Skin Goals - Korektif Wajah',
-                      'Bekas Jerawat, Kemerahan, Kulit Kering, Flek Hitam, Melasma'),
+                  skinGoalKorektifWajah('Skin Goals - Korektif Wajah'),
                   SizedBox(
                     height: 19,
                   ),
-                  skingoalstext('Skin Goals - Korektif Tubuh', '-'),
+                  skinGoalKorektifTubuh('Skin Goals - Korektif Tubuh'),
                   SizedBox(
                     height: 19,
                   ),
-                  skingoalstext('Skin Goals - Augmentation Wajah & Tubuh',
-                      'Hidung Mancung, Rahang Tegas'),
+                  skinGoalAugmentation(
+                      'Skin Goals - Augmentation Wajah & Tubuh'),
                   SizedBox(
                     height: 19,
                   ),
-                  skingoalstext('Pernah Treatment', 'Facial, Peeling'),
+                  skinGoalPernahTreatment('Pernah Treatment'),
                   SizedBox(
                     height: 19,
                   ),
-                  skingoalstext('Anggaran Skincare', 'Rp500.000 - Rp1.000.000'),
+                  skinGoalsBudget(
+                    'Anggaran Skincare',
+                    state.interestData.value.data?.skinGoalsBudget
+                            ?.budgetForSkincare ??
+                        '-',
+                  ),
                   SizedBox(
                     height: 19,
                   ),
-                  skingoalstext(
-                      'Anggaran Treatment', 'Rp500.000 - Rp1.000.000'),
+                  skinGoalsBudget(
+                    'Anggaran Treatment',
+                    state.interestData.value.data?.skinGoalsBudget
+                            ?.budgetForTreatment ??
+                        '-',
+                  ),
                 ],
               ),
             )
@@ -159,7 +169,7 @@ class EditBeautyProfilPage extends StatelessWidget {
     );
   }
 
-  Column skingoalstext(String title1, String title2) {
+  Column skinGoalKorektifWajah(String title1) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -172,13 +182,117 @@ class EditBeautyProfilPage extends StatelessWidget {
             fontSize: 15,
           ),
         ),
-        SizedBox(
-          height: 6,
+        ...?state.interestData.value.data?.skinGoalsFaceCorrective
+            ?.map(
+              (value) => Text(
+                value.nameFaceCorrective.toString(),
+                style: blackTextStyle.copyWith(
+                  fontSize: 15,
+                ),
+              ),
+            )
+            .toList()
+      ],
+    );
+  }
+
+  Column skinGoalKorektifTubuh(String title1) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title1,
+          style: blackRegulerTextStyle.copyWith(
+            color: Color(
+              0xff9B9B9B,
+            ),
+            fontSize: 15,
+          ),
+        ),
+        ...?state.interestData.value.data?.skinGoalsBodyCorrective
+            ?.map(
+              (value) => Text(
+                ' value.nameFaceCorrective.toString()',
+                style: blackTextStyle.copyWith(
+                  fontSize: 15,
+                ),
+              ),
+            )
+            .toList()
+      ],
+    );
+  }
+
+  Column skinGoalAugmentation(String title1) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title1,
+          style: blackRegulerTextStyle.copyWith(
+            color: Color(
+              0xff9B9B9B,
+            ),
+            fontSize: 15,
+          ),
+        ),
+        ...?state.interestData.value.data?.skinGoalsAugmentation
+            ?.map(
+              (value) => Text(
+                'value.nameFaceCorrective.toString()',
+                style: blackTextStyle.copyWith(
+                  fontSize: 15,
+                ),
+              ),
+            )
+            .toList()
+      ],
+    );
+  }
+
+  Column skinGoalPernahTreatment(String title1) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title1,
+          style: blackRegulerTextStyle.copyWith(
+            color: Color(
+              0xff9B9B9B,
+            ),
+            fontSize: 15,
+          ),
+        ),
+        ...?state.interestData.value.data?.skinGoalsHistoryTreatment
+            ?.map(
+              (value) => Text(
+                value.nameHistoryTreatment.toString(),
+                style: blackTextStyle.copyWith(
+                  fontSize: 15,
+                ),
+              ),
+            )
+            .toList()
+      ],
+    );
+  }
+
+  Column skinGoalsBudget(String title1, String title2) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title1,
+          style: blackRegulerTextStyle.copyWith(
+            color: Color(
+              0xff9B9B9B,
+            ),
+            fontSize: 15,
+          ),
         ),
         Text(
           title2,
-          style: blackRegulerTextStyle.copyWith(
-            color: blackColor,
+          style: blackTextStyle.copyWith(
             fontSize: 15,
           ),
         ),
@@ -199,16 +313,11 @@ class EditBeautyProfilPage extends StatelessWidget {
             fontSize: 13,
           ),
         ),
-        InkWell(
-          onTap: () {
-            Get.to(EditBeautyProfilPage());
-          },
-          child: Text(
-            title2,
-            style: blackRegulerTextStyle.copyWith(
-              color: blackColor,
-              fontSize: 13,
-            ),
+        Text(
+          title2,
+          style: blackRegulerTextStyle.copyWith(
+            color: blackColor,
+            fontSize: 13,
           ),
         ),
       ],
