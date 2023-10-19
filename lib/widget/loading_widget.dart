971e74c482c2a44222c8b51747de18cd4fbce84d
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2023-10-20 00:44:30 +0700

    LoadingShimmerWidget

--- lib/widget/loading_widget.dart
+++ lib/widget/loading_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:heystetik_mobileapps/theme/theme.dart';
+import 'package:heystetik_mobileapps/widget/shimmer_widget.dart';
 // import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingWidget extends StatelessWidget {
@@ -46,6 +47,84 @@ class LoadingWidget extends StatelessWidget {
                           ),
                   ],
                 ),
+              )
+            : child,
+      ),
+    );
+  }
+}
+
+class LoadingShimmerWidget extends StatelessWidget {
+  const LoadingShimmerWidget({
+    super.key,
+    this.title = '',
+    required this.isLoading,
+    required this.child,
+    this.top = 0,
+  });
+  final String title;
+  final bool isLoading;
+  final Widget child;
+  final double top;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.transparent,
+      child: Padding(
+        padding: EdgeInsets.only(top: isLoading ? top : 0),
+        child: isLoading
+            ? ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  shimmerWidget(
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.only(right: 5),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 24),
+                      decoration: BoxDecoration(
+                        color: whiteColor,
+                        border: Border.all(color: borderColor),
+                        borderRadius: BorderRadius.circular(7),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: spaceHeigt,
+                  ),
+                  shimmerWidget(
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.only(right: 5),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 24),
+                      decoration: BoxDecoration(
+                        color: whiteColor,
+                        border: Border.all(color: borderColor),
+                        borderRadius: BorderRadius.circular(7),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: spaceHeigt,
+                  ),
+                  shimmerWidget(
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.only(right: 5),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 24),
+                      decoration: BoxDecoration(
+                        color: whiteColor,
+                        border: Border.all(color: borderColor),
+                        borderRadius: BorderRadius.circular(7),
+                      ),
+                    ),
+                  ),
+                ],
               )
             : child,
       ),
