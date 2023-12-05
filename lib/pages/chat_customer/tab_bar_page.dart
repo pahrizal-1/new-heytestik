e9b15b6218495db01a21e75bef9785824eca5a57
Pahrizal Rizal <pahrizal480@gmail.com>
2023-12-05 22:16:33 +0700

    refactkr

--- lib/pages/chat_customer/tab_bar_page.dart
+++ lib/pages/chat_customer/tab_bar_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:from_css_color/from_css_color.dart';
 import 'package:heystetik_mobileapps/pages/chat_customer/detail_perawatan_page.dart';
 import 'package:heystetik_mobileapps/pages/chat_customer/rekomendasi_perawatan_page.dart';
+import 'package:heystetik_mobileapps/pages/chat_customer/resep_digita_rekomendasi_page.dart';
 import 'package:heystetik_mobileapps/pages/chat_customer/tab_bar_galery_page.dart';
 import 'package:heystetik_mobileapps/theme/theme.dart';
 
@@ -63,7 +64,7 @@ class _TabBarChatState extends State<TabBarChat> {
             ),
             Expanded(
               child: TabBarView(children: [
-                RekomendasiPerawatan1Page(id: widget.id),
+                RekomendasiPerawatan1Page(id: ),
                 DetailPerawatanPage(id: widget.id),
                 TabBarGaleryPage(id: widget.id)
               ]),
