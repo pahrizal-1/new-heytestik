f1cad576fb425f006632ffb1d31e4e4238b8282e
Pahrizal Rizal <pahrizal480@gmail.com>
2023-12-21 22:15:15 +0700

    edit keranjang

--- lib/pages/solution/keranjang_page.dart
+++ lib/pages/solution/keranjang_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 import 'package:flutter_svg/svg.dart';
@@ -256,7 +255,7 @@ class _KeranjangPageState extends State<KeranjangPage> {
                               child: Produkheight(
                                 produkId: e.id!.toInt(),
                                 namaBrand: e.skincareDetail!.brand.toString(),
-                                namaProduk: e.name.toString(),
+                                namaProduk: ' e.name.toString()',
                                 diskonProduk: '20',
                                 hargaDiskon:
                                     CurrencyFormat.convertToIdr(e.price, 0),
