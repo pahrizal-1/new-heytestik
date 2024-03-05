aa15388043d2c72ac41ff9a4ec2224e9bc2b0455
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2024-03-05 15:27:37 +0700

    update

--- lib/pages/setings&akun/setings_akun_page.dart
+++ lib/pages/setings&akun/setings_akun_page.dart
@@ -10,7 +10,7 @@ import 'package:heystetik_mobileapps/controller/customer/account/profile_control
 import 'package:heystetik_mobileapps/pages/setings&akun/akun_home_page.dart';
 import 'package:heystetik_mobileapps/pages/setings&akun/daftar_alamat_page.dart';
 import 'package:heystetik_mobileapps/pages/setings&akun/keamanan_akun_page.dart';
-import 'package:heystetik_mobileapps/pages/setings&akun/notifikasi_setings_page.dart';
+import 'package:heystetik_mobileapps/pages/setings&akun/notifikasi_seting_page.dart';
 import 'package:heystetik_mobileapps/pages/setings&akun/rekening_bank_settings_page.dart';
 import 'package:heystetik_mobileapps/widget/appbar_widget.dart';
 import 'package:heystetik_mobileapps/widget/loading_widget.dart';
