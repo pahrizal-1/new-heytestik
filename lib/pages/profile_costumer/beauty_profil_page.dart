6b140315e4dd3df628128f23b6f923feb4a2f37e
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2024-03-13 12:25:36 +0700

    update

--- lib/pages/profile_costumer/beauty_profil_page.dart
+++ lib/pages/profile_costumer/beauty_profil_page.dart
@@ -54,7 +54,7 @@ class BeautyProfil extends StatelessWidget {
             ),
             Obx(
               () => Text(
-                '${stateLocation.myCity.value}${state.age.value == 0 ? '' : ', ${state.age.value} tahun'}',
+                '${stateLocation.myCity.value}${stateLocation.myCity.value.isEmpty && state.age.value == 0 ? ", " : ""}${state.age.value == 0 ? '' : '${state.age.value} tahun'}',
                 style: blackRegulerTextStyle.copyWith(fontSize: 15),
               ),
             ),
